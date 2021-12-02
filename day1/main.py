e49bd815bee0a4fd672785931c716b6f7408d044
Dilyan Kostov <dilyan.kostov@ecs.co.uk>
2021-12-02 13:21:31 +0000

    remove unneeded testcase

--- day1/main.py
+++ day1/main.py
@@ -1,6 +1,5 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from unittest import TestCase
 from assertpy import assert_that
 
 
