5bbb0f67fc4ab7790114af7507136741e33420e1
Dilyan Kostov <44360328+dilyankostov@users.noreply.github.com>
2021-12-15 23:50:41 +0200

    day15 increase efficiency of dijkstra's

--- day15/main.py
+++ day15/main.py
@@ -7,74 +7,70 @@ import time
 start_time = time.time()
 
 
-def extract_data_from_file(file_name: str) -> dict[tuple[int, int], int]:
+def extract_data_from_file(file_name: str) -> tuple[dict[tuple[int, int], int], int, int]:
     data_map: dict[tuple[int, int], int] = {}
     with open(file_name, 'r') as data_file:
-        for i, line in enumerate(data_file):
-            for ii, c, in enumerate(line.strip()):
-                data_map[(i, ii)] = int(c)
-    return data_map
+        for x, line in enumerate(data_file):
+            for y, c, in enumerate(line.strip()):
+                data_map[(x, y)] = int(c)
+    return data_map, x, y
 
 
-def get_adjacent_iteratively(coord, height, width) -> tuple[int, int]:
+def get_adjacent_iteratively(x, y, height, width) -> tuple[int, int]:
     for (i, j) in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        (x, y) = coord
-        if 0 <= x + i <= width and 0 <= y + j <= height:
-            yield x + i, y + j
+        x2, y2 = x + i, y + j
+        if 0 <= x2 <= width and 0 <= y2 <= height:
+            yield x2, y2
 
 
-def search_adjasent(coord_map: dict[tuple[int,int], int], start_coord: tuple[int, int] = (0, 0)) -> int:
-    height_map: int = max(map(lambda i: i[1], coord_map))
-    width_map: int = max(map(lambda i: i[0], coord_map))
+def search_adjasent(coord_map: dict[tuple[int,int], int],
+                    height: int, width: int,
+                    start_coord: tuple[int, int] = (0, 0)) -> int:
     sq: list = [(0, start_coord)]
     visited: set = {start_coord}
     while True:
         min_p, (x, y) = heapq.heappop(sq)
-        if (x, y) == (height_map, width_map):
+        if x == height and y == width:
             return min_p
-        for (x2, y2) in get_adjacent_iteratively((x, y), height=height_map, width=width_map):
+        for x2, y2 in get_adjacent_iteratively(x, y, height=height, width=width):
             if (x2, y2) not in visited:
                 visited.add((x2, y2))
                 heapq.heappush(sq, (min_p + coord_map[x2, y2], (x2, y2)))
 
 
-def expand_map(coord_map):
-    height_map: int = max(map(lambda i: i[1], coord_map)) + 1
-    width_map: int = max(map(lambda i: i[0], coord_map)) + 1
+def expand_map(coord_map: dict[tuple[int,int], int], height: int, width: int):
     new_map = {}
     for i in range(5):
         for j in range(5):
             for ((x, y), v) in coord_map.items():
-                new_map[(width_map * i + x, height_map * j + y)] = int(base_repr(v + i + j, 9)) % 10 or 9
+                new_map[((width + 1) * i + x, (height + 1) * j + y)] = int(base_repr(v + i + j, 9)) % 10 or 9
     return new_map
 
 
 if __name__ == '__main__':
     # PART1 TEST
-    chitons_map = extract_data_from_file("data_test.txt")
-    answer = search_adjasent(chitons_map)
+    chitons_map, map_x, map_y = extract_data_from_file("data_test.txt")
+    answer = search_adjasent(chitons_map, height=map_y, width=map_x)
     assert_that(answer).is_equal_to(40)
 
     # PART1 REAL
-    chitons_map = extract_data_from_file("data_real.txt")
-    answer = search_adjasent(chitons_map)
+    chitons_map, map_x, map_y = extract_data_from_file("data_real.txt")
+    answer = search_adjasent(chitons_map, height=map_y, width=map_x)
     assert_that(answer).is_not_equal_to(40)
 
     # PART2 TEST
-    chitons_map = extract_data_from_file("data_test.txt")
-    chitons_expanded = expand_map(chitons_map)
-    answer = search_adjasent(chitons_expanded)
+    chitons_map, map_x, map_y = extract_data_from_file("data_test.txt")
+    chitons_expanded = expand_map(chitons_map, height=map_y, width=map_x)
+    answer = search_adjasent(chitons_expanded, height=(map_y + 1) * 5 - 1, width=(map_x + 1) * 5 - 1)
     assert_that(answer).is_equal_to(315)
 
     # PART2 REAL
     print("--- %s seconds ---" % (round(time.time() - start_time, 2)))
-    chitons_map = extract_data_from_file("data_real.txt")
+    chitons_map, map_x, map_y = extract_data_from_file("data_real.txt")
     print("--- %s seconds ---" % (round(time.time() - start_time, 2)))
-    chitons_expanded = expand_map(chitons_map)
+    chitons_expanded = expand_map(chitons_map, height=map_y, width=map_x)
     print("--- %s seconds ---" % (round(time.time() - start_time, 2)))
-    # execution_time = timeit.timeit("""search_adjasent(chitons_expanded)""", number=1)
-    # print(execution_time)
-    answer = search_adjasent(chitons_expanded)
+    answer = search_adjasent(chitons_expanded, height=(map_y + 1) * 5 - 1, width=(map_x + 1) * 5 - 1)
     print("--- %s seconds ---" % (round(time.time() - start_time, 2)))
     assert_that(answer).is_not_equal_to(315)
     print(answer)
